5aed7ed2c2c715def2b4016210eeaeab511c2c75
Wesley M oliveira <oliveirawesleyrj@gmail.com>
2020-10-18 18:33:21 -0300

    happy webrtc server

---
+++ src/server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+const socket = require('socket.io')
+const io = socket(server)
+const username = require('username-generator')
+const cors = require('cors');
+
+
+app.use(cors());
+
+app.use(express());
+
+const users={}
+
+io.on('connection', socket => {
+    //gera o id da conexão socket
+    const userid=username.generateUsername('-')
+    if(!users[userid]){
+        users[userid] = socket.id
+    }
+    //emite seu id para outros conectados no socket
+    socket.emit('yourID', userid)
+    io.sockets.emit('allUsers', users)
+    
+    socket.on('disconnect', ()=>{
+        delete users[userid]
+    })
+
+    socket.on('callUser', (data)=>{
+        io.to(users[data.userToCall]).emit('hey', {signal: data.signalData, from: data.from})
+    })
+
+    socket.on('acceptCall', (data)=>{
+        io.to(users[data.to]).emit('callAccepted', data.signal)
+    })
+
+    socket.on('close', (data)=>{
+        io.to(users[data.to]).emit('close')
+    })
+
+    socket.on('rejected', (data)=>{
+        io.to(users[data.to]).emit('rejected')
+    })
+})
+
+const port = process.env.PORT || 8000
+
+server.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
